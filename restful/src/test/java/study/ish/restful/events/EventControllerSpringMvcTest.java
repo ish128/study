003bb8a82d267945f946d0a0f188931fefd6731f
rpcopopqpq@gmail.com <rpcopopqpq@gmail.com>
2019-10-23 00:10:25 +0900

    9. 테스트용 DB와 어플리케이션DB 설정 분리하기

--- restful/src/test/java/study/ish/restful/events/EventControllerSpringMvcTest.java
+++ restful/src/test/java/study/ish/restful/events/EventControllerSpringMvcTest.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import study.ish.restful.RestfulApplication;
@@ -46,6 +47,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocTestConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerSpringMvcTest {
 
   @Autowired
